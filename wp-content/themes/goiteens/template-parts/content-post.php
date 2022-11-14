9cc373b0ad37658fa6f549e5e7c40322595a9f36
Alex <remstroyod@gmail.com>
2022-11-14 10:29:00 +0200

    Fix Bugs v2

--- wp-content/themes/goiteens/template-parts/content-post.php
+++ wp-content/themes/goiteens/template-parts/content-post.php
@@ -13,41 +13,42 @@
 
 <li id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-    <!-- image -->
     <a href="<?php echo get_permalink(); ?>">
+
+        <!-- image -->
         <?php if (has_post_thumbnail()) {
             the_post_thumbnail('post-thumbnail', ['class' => 'publication-image']);
         } else { ?>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/blog/Group_126.png" alt="" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/blog/Group_126.png" alt="<?= get_the_title() ?>" />
         <?php } ?>
-    </a>
-    <!-- end image -->
+        <!-- end image -->
 
-    <?php
-    $cats = wp_get_post_categories(get_the_ID(), ['fields' => 'all']);
-    ?>
-    <?php if( $cats ) { ?>
-        <!-- label -->
-        <p class="publication-label">
-            <?php foreach ( $cats as $cat ) { ?>
-                <?= $cat->name ?>
+        <?php
+        $cats = wp_get_post_categories(get_the_ID(), ['fields' => 'all']);
+        ?>
+        <?php if( $cats ) { ?>
+            <!-- label -->
+            <p class="publication-label">
+                <?php foreach ( $cats as $cat ) { ?>
+                    <?= $cat->name ?>
+                <?php } ?>
+            </p>
+            <!-- end label -->
+        <?php } ?>
+
+        <!-- description -->
+        <div class="publication-description">
+
+            <?php the_title('<h3 class="publication-description__title">', '</h3>'); ?>
+
+            <?php if( has_excerpt() ) { ?>
+                <p class="publication-description__text"><?= get_the_excerpt() ?></p>
             <?php } ?>
-        </p>
-        <!-- end label -->
-    <?php } ?>
 
-    <!-- description -->
-    <div class="publication-description">
+            <div class="publication-description__link"><?= _e( 'Читати далі', 'goiteens' ) ?> ›</div>
 
-        <?php the_title('<h3 class="publication-description__title">', '</h3>'); ?>
+        </div>
+        <!-- end description -->
 
-        <?php if( has_excerpt() ) { ?>
-            <p class="publication-description__text"><?= get_the_excerpt() ?></p>
-        <?php } ?>
-
-        <a class="publication-description__link" href="<?php echo get_permalink(); ?>"><?= _e( 'Читати далі', 'goiteens' ) ?> ›</a>
-
-    </div>
-    <!-- end description -->
-
+    </a>
 </li><!-- #post-<?php the_ID(); ?> -->
