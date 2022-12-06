99d39d581830a5764e97857d03490a34651a1a11
Alex <remstroyod@gmail.com>
2022-12-06 14:44:20 +0200

    Add Itmarathon New Year Page

---
+++ wp-content/themes/goiteens/template-parts/itmarathon/expect.php
@@ -0,0 +1,13 @@
+<!-- expectation -->
+<section class="section itm_newyear__expect" id="expect">
+
+    <!-- container -->
+    <div class="container">
+
+        <!-- TODO тут HTML разметка -->
+
+    </div>
+    <!-- end container -->
+
+</section>
+<!-- end expectation -->
--- wp-content/themes/goiteens/templates/tmpl-itmarathon-page.php
+++ wp-content/themes/goiteens/templates/tmpl-itmarathon-page.php
@@ -14,22 +14,39 @@ function goiteens_template_page_itmarathon_enqueue_styles()
 
 }
 
+if( is_page( 'marathon_newyear' ) )
+{
+    add_filter('body_class', 'goiteens_template_page_itmarathon_add_body_classes');
+    function goiteens_template_page_itmarathon_add_body_classes($classes) {
+        $classes[] = 'itm_newyear';
+        return $classes;
+    }
+}
+
 get_header();
 ?>
 
 <?php get_template_part( 'template-parts/itmarathon/header'  ); ?>
 
 <main>
-		 <?php get_template_part( 'template-parts/itmarathon/profession', get_post_type()  ); ?>
-		 <?php get_template_part( 'template-parts/itmarathon/for', get_post_type()  ); ?>
-		<?php get_template_part( 'template-parts/itmarathon/result', get_post_type()  ); ?>
-		 <?php get_template_part( 'template-parts/itmarathon/study', get_post_type()  ); ?>
-		<?php get_template_part( 'template-parts/itmarathon/projects', get_post_type()  ); ?>
-		<?php get_template_part( 'template-parts/itmarathon/how', get_post_type()  ); ?>
-		<?php get_template_part( 'template-parts/itmarathon/schedule', get_post_type()  ); ?>
-		<?php get_template_part( 'template-parts/itmarathon/lesson', get_post_type()  ); ?>
-		<?php get_template_part( 'template-parts/itmarathon/why', get_post_type()  ); ?>
-		<?php get_template_part( 'template-parts/itmarathon/help', get_post_type()  ); ?>
+
+    <?php
+    if( is_page( 'marathon_newyear' ) )
+    {
+        get_template_part( 'template-parts/itmarathon/expect' );
+    }
+    ?>
+
+    <?php get_template_part( 'template-parts/itmarathon/profession'  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/for', get_post_type()  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/result', get_post_type()  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/study', get_post_type()  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/projects', get_post_type()  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/how', get_post_type()  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/schedule', get_post_type()  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/lesson', get_post_type()  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/why', get_post_type()  ); ?>
+    <?php get_template_part( 'template-parts/itmarathon/help', get_post_type()  ); ?>
 
 </main>
 <?php get_template_part( 'template-parts/main-footer'  ); ?>
