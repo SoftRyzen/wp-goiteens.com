3fed83ca21821d4509a3d9ffc2663b4303d760f5
Ekaterina Aleksandrovych <ealeksandrovych@gmail.com>
2022-12-07 05:50:51 +0200

    start files and styles

--- wp-content/themes/goiteens/template-parts/itmarathon/expect.php
+++ wp-content/themes/goiteens/template-parts/itmarathon/expect.php
@@ -2,12 +2,40 @@
 <section class="section itm_newyear__expect" id="expect">
 
     <!-- container -->
+    <div class="expect_wrapper">
     <div class="container">
-
+<h2 hidden class="expect_title">holiday marathon schedule</h2>
+<p class="expect_question">Що ми будем робити на марфоні ?</p>
+<ul class="expect_days-list">
+    <li class="expect_days-item">
+        <h3 >день 1</h3>
+        <p>Створення новорічної листівки за допомогою Canva зі смартфону</p>
+    </li>
+    <li class="expect_days-item">
+        <h3>день 2</h3>
+        <p>Підготовка до Нового Року в метавсесвіті Minecraft за допомогою Minecraft Education</p>
+    </li>
+    <li class="expect_days-item">
+        <h3>день 3</h3>
+        <p>Створюємо новорічні колажі або як обробити фотографію і додати магії огоньочків</p>
+    </li>
+    <li class="expect_days-item">
+        <h3>день 4</h3>
+        <p>Створення новорічного костюму для персонажів в навчальному середовищі ROBLOX</p>
+    </li>
+    <li class="expect_days-item">
+        <h3>день 5</h3>
+        <p>Створюємо новорічний наряд в роблокс</p>
+    </li>
+</ul>
+<p class="expect_">і це лише перші 5 днів - далі більше!</p>
+<strong class="expect_suggestion">Відкрийте нові таланти
+вашої дитини!</strong>
+<button class="expect_registration-btn">зареєструватися</button>
         <!-- TODO тут HTML разметка -->
 
     </div>
     <!-- end container -->
-
+    </div>
 </section>
 <!-- end expectation -->
