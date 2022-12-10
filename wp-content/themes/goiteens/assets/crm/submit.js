ec22a74299e068aef690e3cb696ddc49372a7855
Alex <remstroyod@gmail.com>
2022-12-10 10:19:51 +0200

    Add Capi Settings New Branch

--- wp-content/themes/goiteens/assets/crm/submit.js
+++ wp-content/themes/goiteens/assets/crm/submit.js
@@ -22,7 +22,7 @@ function generateData(name, phone = null, email = null, ipData = {}) {
     leadFBP: getCookie('_fbp'),
     leadFBC: getCookie('_fbc'),
     leadIP: ipData.ip || '',
-    leadapi: "MarLeadAPI",
+    leadapi: window.leadapi || '',
   };
   return ensureUtmData(data);
 }
