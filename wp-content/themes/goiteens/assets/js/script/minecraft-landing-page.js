569abc106bc10c0614662b44fb26f15974086d0f
Ekaterina Aleksandrovych <ealeksandrovych@gmail.com>
2022-12-21 10:58:26 +0200

    final fix v1

--- wp-content/themes/goiteens/assets/js/script/minecraft-landing-page.js
+++ wp-content/themes/goiteens/assets/js/script/minecraft-landing-page.js
@@ -37,17 +37,40 @@ jQuery( function( $ ) {
 		slider: function()
 		{
 
-			const swiper = new Swiper('.swiper', {
+			 const swiper = new Swiper('.swiper', {
 				loop: true,
+				// initialSlide:0,
+				slidesPerView: 1,
+				spaceBetween: 32,
+				centeredSlides:true,
+				slidesPerGroup:1,
 				//TODO This Settings
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev',
+				  },
+				  pagination: {
+					el: ".swiper-pagination",
+					dynamicBullets: true,
+					clickable: true,
+				  },
+				//   Responsive breakpoints
+				  breakpoints: {
+					768: {
+					  slidesPerView: 2,
+					},
+					1280: {
+					  slidesPerView: 3,
+					}
+				},
+					
 			});
-
 		},
 
 	}
 
 	MinecraftLandingPage.init()
-
+	
 
 
 });
